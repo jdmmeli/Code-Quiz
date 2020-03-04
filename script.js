e9d0503c23cabc6b29d74d3111e7917be4621484
Jason Meli <jdmmeli@yahoo.com>
2020-03-04 14:54:13 -0500

    made questions array

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+var questions = [{
+    title: "Arrays in javascript can be used to store?",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+},
+{
+    title: "A very useful tool used during development and debugging for printing content to the debugger is?",
+    choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+    answer: "console log"
+},
+{
+    title: " The condition in an if / else statement is enclosed within?",
+    choices: ["quotes", "curly brackets", "parantheses", "square brackets"],
+    answer: "parantheses"
+},
+{
+    title: "String variables must be enclosed within __________ when being assigned to variables?",
+    choices: ["commas", "curly brackets", "quotes", "parantheses"],
+    answer: "quotes"
+},
+{
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+}
+]
+
+
+
 var score = 0;
 var currentQuestion = -1;
 var timeLeft = 0;
